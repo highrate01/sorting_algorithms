9da5165383d0545099f695893de53bfc52236d0f
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-03-05 10:34:27 -0800

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,8 +1,8 @@
 #include "sort.h"
 
 /**
- * bubble_sort - This function compares the consequtive of data
- * and swap numbers that are out of place(it sorts)
+ * bubble_sort - This function swaps an array of integer
+ * using the buble sort algorithm
  *
  * @array: array to be sorted
  * @size: it reps the total number of datas in the array
@@ -13,17 +13,24 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+	int swapped;
 
+	if (array == NULL || size < 2)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
+		swapped = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				swap_int(&array[j], &array[j + 1]);
 				print_array(array, size);
+				swapped = 1;
 			}
 		}
+		if (swapped == 0)
+			break;
 	}
 }
 
