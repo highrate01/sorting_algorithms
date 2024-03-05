3b4e0458169dcdd1cf0dc6a23b4dcd7147cdba38
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-03-05 13:09:42 -0800

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,35 +11,43 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *ptr, *temp, *prev_temp, *next_ptr;
+	listint_t *current, *ptr, *temp;
 
 	if ((list == NULL && *list == NULL) || (*list)->next == NULL)
 		return;
-	current = (*list)->next;
-	while (current != NULL)
+	ptr = (*list)->next;
+	while (ptr != NULL)
 	{
-		ptr = current;
-		temp = current->prev;
+		temp = ptr->next;
+		current = ptr->prev;
 
-		while (temp != NULL && temp->n > ptr->n)
+		while (current != NULL && ptr->n < current->n)
 		{
-			prev_temp = temp->prev;
-			next_ptr = ptr->next;
-			if (prev_temp != NULL)
-				prev_temp->next = ptr;
-			if (next_ptr != NULL)
-				next_ptr->prev = temp;
-			ptr->prev = prev_temp;
-			temp->prev = ptr;
-			temp->next = next_ptr;
-			ptr->next = temp;
-
-			if (ptr->prev == NULL)
-				*list = ptr;
-
-			temp = ptr->prev;
-			print_list(*list);
+			swap_nodes(list, &current, ptr);
+			print_list((const listint_t *)*list);
 		}
-		current = current->next;
+		ptr = temp;
 	}
+}
+/**
+ * swap_nodes - swaps two nodes in doubly linked list
+ *
+ * @head: pointer to the head of the list
+ * @node1: pointer to first node
+ * @node2: pointer to second node
+ * Return: nothing
+ */
+void swap_nodes(listint_t **head, listint_t **node1, listint_t *node2)
+{
+	(*node1)->next = node2->next;
+	if (node2->next != NULL)
+		node2->next->prev = *node1;
+	node2->prev = (*node1)->prev;
+	node2->next = *node1;
+	if ((*node1)->prev != NULL)
+		(*node1)->prev->next = node2;
+	else
+		*head = node2;
+	(*node1)->prev = node2;
+	*node1 = node2->prev;
 }
