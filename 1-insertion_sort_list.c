2b6160b1782999ed7d371195c69b9c37ef117978
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-03-05 03:18:58 -0800

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,10 +12,10 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *ptr, *temp;
 
-	current = (*list)->next;
 	if ((list == NULL && *list == NULL) || (*list)->next == NULL)
 		return;
 
+	current = (*list)->next;
 	while (current != NULL)
 	{
 		ptr = current;
@@ -34,8 +34,8 @@ void insertion_sort_list(listint_t **list)
 
 			if (ptr->prev == NULL)
 				*list = ptr;
-			temp = ptr->prev;
 
+			temp = ptr->prev;
 			print_list(*list);
 		}
 		current = current->next;
